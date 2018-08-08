688ad49e1a9042315a2f1584a2bb4baefca659a7
Christine Xiang <yutingcxiang@gmail.com>
2018-08-08 22:34:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,19 @@
 class Owner
+<<<<<<< HEAD
   attr_accessor :name, :pets
+=======
+  attr_accessor :name, :fishes, :dogs, :cats, :pets
+>>>>>>> 35d35642085e3435c08fcad8ffd08919aee7be4d
   attr_reader :species
   @@all = []
   
   def initialize(name)
     @name = name
     @@all << self
+<<<<<<< HEAD
     @pets = {:fishes => [], :dogs => [], :cats => []}
+=======
+>>>>>>> 35d35642085e3435c08fcad8ffd08919aee7be4d
   end
   
   def self.all
@@ -29,6 +36,7 @@ class Owner
     return "I am a #{species}."
   end
   
+<<<<<<< HEAD
   def buy_fish(name)
     new_fish = Fish.new(name)
     pets[:fishes] << new_fish
@@ -68,4 +76,17 @@ class Owner
   def list_pets
     return "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
+=======
+  def pets
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    return pets
+  end
+  
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
+  
+  
+>>>>>>> 35d35642085e3435c08fcad8ffd08919aee7be4d
 end
